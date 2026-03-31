27c535b97577390b94a11dcebb5b746ca3486f73
iheb dridi <drdrhbhb5@gmail.com>
2026-03-31 12:36:11 +0200

    DB reset through run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,15 @@ export DATABASE_URL=${POSTGRESQL_ADDON_URI}
 
 # Note: otree migrate is not available in this oTree CLI; run ALTER TABLE manually if you add new Player fields.
 # Note: otree resetdb fails on PostGIS DBs (cannot drop spatial_ref_sys). To clear data, use truncate_otree_tables.sql or run fix_wide_export_one_session.sql for one session.
+#!/usr/bin/env bash
+set -e
+yes | otree resetdb
 
 otree prodserver 9000
+
+
+
+
 
 # DO $$
 # DECLARE
